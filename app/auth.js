42ee28e30c838164d34e91b50331a402fc85fac3
megos <gmegos.27@gmail.com>
2016-03-13 02:39:54 +0900

    OAuthをモジュール化

---
+++ app/auth.js
@@ -0,0 +1,42 @@
+var browserWindow = require('browser-window');
+var storage = require('electron-json-storage');
+
+var TwitterAuth = function(consumerKey, consumerSecret) {
+  this.consumerKey = consumerKey;
+  this.consumerSecret = consumerSecret;
+};
+
+TwitterAuth.prototype.getTwitterAuth = function() {
+  var twitterAPI = require('node-twitter-api');
+
+  var twitter = new twitterAPI({
+    consumerKey: this.consumerKey,
+    consumerSecret: this.consumerSecret,
+    callback: 'http://tmegos.hatenablog.jp' // 別にどこでもよい
+  });
+
+
+  twitter.getRequestToken(function(error, requestToken, requestTokenSecret) {
+    var url = twitter.getAuthUrl(requestToken);
+    var loginWindow = new browserWindow({width: 800, height: 600});
+    loginWindow.webContents.on('will-navigate', function (event, url) {
+      var matched;
+      if(matched = url.match(/\?oauth_token=([^&]*)&oauth_verifier=([^&]*)/)) {
+        twitter.getAccessToken(requestToken, requestTokenSecret, matched[2], function(error, accessToken, accessTokenSecret){
+          var auth = {
+            accessToken: accessToken,
+            accessTokenSecret: accessTokenSecret
+          };
+          storage.set('auth', auth, function(error) {
+            if (error) throw error;
+          });
+        });
+      }
+      event.preventDefault();
+      loginWindow.close();
+    });
+    loginWindow.loadUrl(url);
+  });
+};
+
+module.exports = TwitterAuth;
--- index.js
+++ index.js
@@ -2,38 +2,22 @@
 
 var app = require('app');
 var browserWindow = require('browser-window');
+var storage = require('electron-json-storage');
 var settings = require('./app/settings');
 
 require('crash-reporter').start();
 
 var mainWindow = null;
 
-var twitterAPI = require('node-twitter-api');
+var twitterAuth = require('./app/auth');
+twitterAuth = new twitterAuth(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET);
 
-var twitter = new twitterAPI({
-  consumerKey: settings.TWITTER_CONSUMER_KEY,
-  consumerSecret: settings.TWITTER_CONSUMER_SECRET,
-  callback: 'http://tmegos.hatenablog.jp' // 別にどこでもよい
-});
+storage.get('auth', function(error, data) {
+	if (error) throw error;
 
-
-twitter.getRequestToken(function(error, requestToken, requestTokenSecret) {
-  var url = twitter.getAuthUrl(requestToken);
-  var loginWindow = new browserWindow({width: 800, height: 600});
-  loginWindow.webContents.on('will-navigate', function (event, url) {
-  	console.log(url);
-    // https://www.google.co.jp/?oauth_token=...&oauth_verifier=...のようなURLが渡ってくる.
-    var matched;
-    if(matched = url.match(/\?oauth_token=([^&]*)&oauth_verifier=([^&]*)/)) {
-      twitter.getAccessToken(requestToken, requestTokenSecret, matched[2], function(error, accessToken, accessTokenSecret){
-        console.log('accessToken', accessToken);
-        console.log('accessTokenSecret', accessTokenSecret);
-      });
-    }
-    event.preventDefault();
-    loginWindow.close();
-  });
-  loginWindow.loadUrl(url);
+	if (Object.keys(data).length === 0) {
+		twitterAuth.getTwitterAuth();
+	}
 });
 
 app.on('window-all-closed', function() {
