72254ae59b277c629c52a0c20c244d2d82f32eb7
megos <gmegos.27@gmail.com>
2016-03-21 17:49:38 +0900

    コードを簡略化

--- app/main.js
+++ app/main.js
@@ -5,19 +5,14 @@ var settings = require('../app/settings');
 var Client = require('../app/client');
 var Timeline = require('../app/timeline');
 var Parser = require('../app/parser');
-var accessTokenKey = '';
-var accessTokenSecret = '';
-var client = '';
 
 var auth = JSON.parse(localStorage.getItem('auth'));
-accessTokenKey = auth.accessTokenKey;
-accessTokenSecret = auth.accessTokenSecret;
-console.log(auth);
-client = new Client(settings.TWITTER_CONSUMER_KEY,
+
+var client = new Client(settings.TWITTER_CONSUMER_KEY,
                     settings.TWITTER_CONSUMER_SECRET,
-                    accessTokenKey,
-                    accessTokenSecret);
-console.log(client);
+                    auth.accessTokenKey,
+                    auth.accessTokenSecret);
+
 var twitterClient = client.getClient();
 
 var timeline = new Timeline(twitterClient);
