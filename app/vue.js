d67fcca1dfbff2e3505dd0e8b3769b51dc3fa490
megos <gmegos.27@gmail.com>
2016-03-22 16:46:09 +0900

    settimeout廃止

--- app/vue.js
+++ app/vue.js
@@ -1,73 +1,71 @@
-setTimeout(function() {
-  var tweetComponent = Vue.extend({
-    props: ['tweet'],
-    template: '#tweet-component',
-    methods: {
-      openImageWindow: function(url, height, width, event) {
-        // TODO: マジックナンバーをどうにかする
-        var titleBarHeight = 22;
-        var imageWindow = new BrowserWindow({
-          height: height + titleBarHeight,
-          width: width
-        });
-        imageWindow.loadUrl(url);
-        imageWindow.on('closed', function() {
-          imageWindow = null;
-        });
-      }
-    }
-  });
-  Vue.component('timeline-home', tweetComponent);
-  Vue.component('timeline-mentions', tweetComponent);
-
-  var timelineVue = new Vue({
-    el: '#main',
-    data: {
-      tweets: [],
-      mentions: [],
-      tweettext: ''
-    },
-    created: function() {
-      var self = this;
-
-      timeline.getTimeline(function(tweetsRow) {
-        for (var i = 0; i < tweetsRow.length; i++) {
-          self.tweets.push(parser.tweetParse(tweetsRow[i]));
-        }
-        console.log(self.tweets);
+var tweetComponent = Vue.extend({
+  props: ['tweet'],
+  template: '#tweet-component',
+  methods: {
+    openImageWindow: function(url, height, width, event) {
+      // TODO: マジックナンバーをどうにかする
+      var titleBarHeight = 22;
+      var imageWindow = new BrowserWindow({
+        height: height + titleBarHeight,
+        width: width
       });
+      imageWindow.loadUrl(url);
+      imageWindow.on('closed', function() {
+        imageWindow = null;
+      });
+    }
+  }
+});
+Vue.component('timeline-home', tweetComponent);
+Vue.component('timeline-mentions', tweetComponent);
 
-      timeline.getUserStream(function(data) {
+var timelineVue = new Vue({
+  el: '#main',
+  data: {
+    tweets: [],
+    mentions: [],
+    tweettext: ''
+  },
+  created: function() {
+    var self = this;
+
+    timeline.getTimeline(function(tweetsRow) {
+      for (var i = 0; i < tweetsRow.length; i++) {
+        self.tweets.push(parser.tweetParse(tweetsRow[i]));
+      }
+      console.log(self.tweets);
+    });
+
+    timeline.getUserStream(function(data) {
+      console.log(data);
+      if (data.created_at != null && data.text != null) {
+        for (var i = 0; i < self.tweets.length; i++) {
+          self.tweets[i] = parser.setRelativeCreatedAt(self.tweets[i]);
+        }
+        self.tweets.unshift(parser.tweetParse(data));
+      } else if (data.delete != null) {
         console.log(data);
-        if (data.created_at != null && data.text != null) {
-          for (var i = 0; i < self.tweets.length; i++) {
-            self.tweets[i] = parser.setRelativeCreatedAt(self.tweets[i]);
-          }
-          self.tweets.unshift(parser.tweetParse(data));
-        } else if (data.delete != null) {
-          console.log(data);
-          for (var i = 0; i < self.tweets.length; i++) {
-            // 削除tweet id確認
-            if ((self.tweets[i].id == data.delete.status.id) && (self.tweets[i].user.id == data.delete.status.user_id)) {
-              self.tweets.splice(i, 1);
-            }
+        for (var i = 0; i < self.tweets.length; i++) {
+          // 削除tweet id確認
+          if ((self.tweets[i].id == data.delete.status.id) && (self.tweets[i].user.id == data.delete.status.user_id)) {
+            self.tweets.splice(i, 1);
           }
         }
-      });
-
-      timeline.getMentionsTimeline(function(tweetsRow) {
-        for (var i = 0; i < tweetsRow.length; i++) {
-          self.mentions.push(parser.tweetParse(tweetsRow[i]));
-        }
-      });
-    },
-    methods: {
-      tweet: function(event) {
-        var self = this;
-        timeline.postTweet(this.tweettext, function(callback) {
-          self.tweettext = '';
-        });
       }
+    });
+
+    timeline.getMentionsTimeline(function(tweetsRow) {
+      for (var i = 0; i < tweetsRow.length; i++) {
+        self.mentions.push(parser.tweetParse(tweetsRow[i]));
+      }
+    });
+  },
+  methods: {
+    tweet: function(event) {
+      var self = this;
+      timeline.postTweet(this.tweettext, function(callback) {
+        self.tweettext = '';
+      });
     }
-  });
-}, 0);
+  }
+});
