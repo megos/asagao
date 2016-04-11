f8da87e1a59dfc953b4bee442b4a6d6751bd7675
megos <gmegos.27@gmail.com>
2016-04-11 23:17:52 +0900

    メニュー作成

--- index.js
+++ index.js
@@ -3,27 +3,60 @@
 var app           = require('app');
 var browserWindow = require('browser-window');
 var shell         = require('shell');
+var Menu          = require('menu');
 
 require('crash-reporter').start();
 
 var mainWindow = null;
 
 app.on('window-all-closed', function() {
-	if (process.platform != 'darwin') {
-		app.quit();
-	}
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('ready', function() {
-	mainWindow = new browserWindow({width: 500, height: 600, webPreferences: {webSecurity: false}});
-	mainWindow.loadURL('file://' + __dirname + '/view/login.html');
+  mainWindow = new browserWindow({width: 500, height: 600, webPreferences: {webSecurity: false}});
+  mainWindow.loadURL('file://' + __dirname + '/view/login.html');
 
-	mainWindow.on('closed', function() {
-		mainWindow = null;
-	});
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
 
-	mainWindow.webContents.on('new-window', function(event, url) {
-		event.preventDefault();
-		shell.openExternal(url);
-	});
+  mainWindow.webContents.on('new-window', function(event, url) {
+    event.preventDefault();
+    shell.openExternal(url);
+  });
+
+  // Menu.setApplicationMenu(menu);
 });
+
+var template = [
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+      },
+      {
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+      },
+      {
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+      },
+      {
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        role: 'selectall'
+      }
+    ]
+  }
+];
+
+var menu = Menu.buildFromTemplate(template);
