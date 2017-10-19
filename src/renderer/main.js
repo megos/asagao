8cdfaafa16208ae283a44e2f8b5c6098528bf94b
megos <gmegos.27@gmail.com>
2017-10-19 22:55:22 +0900

    Import OnsenUI

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,4 +1,8 @@
+import 'onsenui/css/onsenui.css'
+import 'onsenui/css/onsen-css-components.css'
+
 import Vue from 'vue'
+import VueOnsen from 'vue-onsenui'
 import axios from 'axios'
 
 import App from './App'
@@ -8,6 +12,8 @@ import store from './store'
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+Vue.use(VueOnsen)
 
 /* eslint-disable no-new */
 new Vue({
