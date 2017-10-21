8c66403208a25383eca6d5aede9471b574faeb43
megos <gmegos.27@gmail.com>
2017-10-22 01:16:46 +0900

    Modified props access

--- src/renderer/components/TweetItem.vue
+++ src/renderer/components/TweetItem.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-ons-list-item :key="this.$props.idx" v-if="this.$props.tweet">
-    {{ this.$props.tweet.full_text }}
+  <v-ons-list-item :key="idx" v-if="tweet">
+    {{ tweet.full_text }}
   </v-ons-list-item>
 </template>
 
