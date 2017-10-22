251848827464e80d101b86126bf7505e0c0195a9
megos <gmegos.27@gmail.com>
2017-10-22 17:18:32 +0900

    Delete axios

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,14 +3,12 @@ import 'onsenui/css/onsen-css-components.css'
 
 import Vue from 'vue'
 import VueOnsen from 'vue-onsenui'
-import axios from 'axios'
 
 import App from './App'
 import router from './router'
 import store from './store'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
-Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.use(VueOnsen)
