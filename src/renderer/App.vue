d11e69f534c88f28d6e7970faf1b980ca6c6e7bd
megos <gmegos.27@gmail.com>
2017-10-27 23:26:56 +0900

    Add cron job

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -10,10 +10,24 @@
 </template>
 
 <script>
+  import { CronJob } from 'cron'
+
   export default {
     name: 'asagao',
-    mounted: function () {
-      this.$store.dispatch('fetchTimeline')
+    created () {
+      this.startJob()
+    },
+    methods: {
+      startJob () {
+        const self = this
+        const job = new CronJob({
+          cronTime: '0 */1 * * * *',
+          onTick: () => self.$store.dispatch('fetchTimeline'),
+          start: false,
+          runOnInit: true
+        })
+        job.start()
+      }
     }
   }
 </script>
