ede1ce6a003fcfb76ac057a361de0cb0d9736191
megos <gmegos.27@gmail.com>
2017-10-28 14:31:48 +0900

    Create tweet input

---
+++ src/renderer/components/TweetInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-ons-page>
+    <v-ons-list>
+      <v-ons-list-item>
+        <div class="center">
+          <v-ons-input float
+            v-model="tweet"
+          />
+        </div>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <div class="right">
+          <v-ons-button>Tweet</v-ons-button>
+        </div>
+      </v-ons-list-item>
+    </v-ons-list>
+  </v-ons-page>
+</template>
+
+<script>
+  export default {
+    name: 'tweet-input',
+    data () {
+      return {
+        tweet: ''
+      }
+    }
+  }
+</script>
