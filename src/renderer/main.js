86303b70f7ea25ac918a5088a0564366537ab402
megos <gmegos.27@gmail.com>
2017-11-01 10:50:44 +0900

    Import log4js

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,12 +4,16 @@ import 'onsenui/css/onsen-css-components.css'
 import Vue from 'vue'
 import VueOnsen from 'vue-onsenui'
 
+import log4js from 'log4js'
+
 import App from './App'
 import router from './router'
 import store from './store'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
+
+Vue.logger = Vue.prototype.$logger = log4js.getLogger()
 
 Vue.use(VueOnsen)
 
