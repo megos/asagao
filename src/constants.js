1f07c5e121e76e8ef853fe3eb39416829c05f4b5
megos <gmegos.27@gmail.com>
2019-01-05 21:38:02 +0900

    Remove files

--- src/constants.js
+++
@@ -1,8 +0,0 @@
-export const credentials = {
-  CONSUMER_KEY: 'PsQsR08SaJ38b0zEiU7DeWZDy',
-  CONSUMER_SECRET: 'fNzbIA4Pqo3Avr558vwSJXL4YMAuTurMb9HG8lu6sp7BlJSimf'
-}
-
-export const keys = {
-  OAUTH_TOKEN: 'oauthToken'
-}
